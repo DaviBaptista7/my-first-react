b1651791b87bd1fe28c022308b312ca62bbc9d5c
Davi <baptistadossantosdavi@gmail.com>
2025-09-26 19:03:09 -0300

    Usando o children

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import { Card } from "./components/Card";
+import { Hello } from "./components/Hello";
+
 const App = () => {
   return (
     <main className = "min-h-dvh grid place-items-center bg-slate-50">
-    <h1 className = "text-3x1 font-bold tex-slate-800">
+    <h1 className = "text-3xl font-bold tex-slate-800">
       Hello Tailwind + React!
       </h1>
       <div className=" container mx-auto p-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        <div className ="rounded-2x1 border p-4 bg-white shadow-sm">Card A</div>
-        <div className ="rounded-2x1 border p-4 bg-white shadow-sm">Card B</div>
-        <div className ="rounded-2x1 border p-4 bg-white shadow-sm">Card C</div>
+        <Card title="Card A">
+          <p>Oi, eu sou o filho</p>
+        </Card>
+        <Card title="Card B">
+          <p>Oi, eu sou o filho</p>
+        </Card>
+        <Card title="Card C">
+          <p>Oi, eu sou o filho</p>
+        </Card>
       </div>
+      <a href="#" className="inline-block px-4 py-2 rounded-lg border hover:bg-slate-300 focus:ring-2 focus:ring-blue-500 transition-all">
+Link com foco
+      </a>
+      <Hello name="Davi"/>
     </main>
   );
 }
